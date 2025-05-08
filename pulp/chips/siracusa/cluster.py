371772c3af4d2b7cde029f2787fb2b16716cb76e
Run Wang <52746141+SamanthaWangdl@users.noreply.github.com>
2025-05-08 19:49:25 +0000

    Add Redmule to Siracusa

--- pulp/chips/siracusa/cluster.py
+++ pulp/chips/siracusa/cluster.py
@@ -25,6 +25,7 @@ from pulp.mchan.mchan_v7 import Mchan
 from pulp.timer.timer_v2 import Timer
 from pulp.cluster.cluster_control_v2 import Cluster_control
 from pulp.neureka.neureka import Neureka
+from pulp.chips.siracusa.light_redmule import LightRedmule
 from pulp.icache_ctrl.icache_ctrl_v2 import Icache_ctrl
 
 
@@ -122,6 +123,26 @@ class Cluster(st.Component):
 
         # NEUREKA
         neureka = Neureka(self, 'neureka')
+        
+        # Redmule
+        redmule_config = self.get_property('peripherals/redmule')
+
+        redmule_params = {
+            'tcdm_bank_width': redmule_config.get('tcdm_bank_width', 4),
+            'tcdm_bank_number': redmule_config.get('tcdm_bank_number', 16),
+            'elem_size': redmule_config.get('elem_size', 4),
+            'ce_height': redmule_config.get('ce_height', 12),
+            'ce_width': redmule_config.get('ce_width', 4),
+            'ce_pipe': redmule_config.get('ce_pipe', 3),
+            'queue_depth': redmule_config.get('queue_depth', 8),
+            'fold_tiles_mapping': redmule_config.get('fold_tiles_mapping', 0)
+        }
+
+        redmule = LightRedmule(
+            self,
+            'redmule',
+            **redmule_params  
+        )
 
         # Icache controller
         icache_ctrl = Icache_ctrl(self, 'icache_ctrl')
@@ -133,6 +154,12 @@ class Cluster(st.Component):
         #
         # Bindings
         #
+
+        # Redmule
+        periph_ico.add_mapping('redmule', **self._reloc_mapping(self.get_property('peripherals/redmule/mapping')))
+
+        self.bind(periph_ico, 'redmule', redmule, 'input')
+        self.bind(redmule, 'tcdm', l1, 'neureka_in')
 
         # L1 subsystem
         for i in range(0, nb_pe):
